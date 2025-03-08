5d0f7f3be694361c3fd2b5040519d607e95349aa
srishti <srishtimanna6@gmail.com>
2025-03-08 09:14:58 +0530

    remove duplicates from sorted array

---
+++ dsa/remove duplicates from sorted array.cpp
@@ -0,0 +1,18 @@
+// question: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            set <int> set;
+            for(int i=0; i<nums.size(); i++){
+                set.insert(nums[i]);
+            }
+    
+            int k = set.size();
+            int j = 0;
+            for (int x: set) {
+                nums[j++] = x;
+            }        
+            return k;
+        }
+    };

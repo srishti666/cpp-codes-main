c97d14f0b04f664826da5d5e255cb17946178001
srishti <srishtimanna6@gmail.com>
2025-03-10 09:36:42 +0530

    move zeros: solution 2

--- dsa/move zeros.cpp
+++ dsa/move zeros.cpp
@@ -19,17 +19,47 @@ class Solution {
         }
     };
 
-//other solution : Time complexity: O(n).
-// Space complexity: O(1).
+
+    //solution: 2
     class Solution {
         public:
             void moveZeroes(vector<int>& nums) {
-                int j = 0;
-                for (int i = 0; i < nums.size(); i++) {
-                    if (nums[i] != 0) {
-                        swap(nums[i], nums[j]);
-                        j++;
+                vector<int> temp;
+                for(int i=0; i<nums.size(); i++){
+                    if(nums[i] != 0){
+                        temp.push_back(nums[i]);
                     }
+                }
+                //size of temp array(number of non zero element)
+                int nz = temp.size();
+        
+                //putting the non zero element back in original array
+                for(int i=0; i<nz; i++){
+                    nums[i] = temp[i];
+                }
+        
+                //filling the remaining space with zeros
+                for(int i=nz; i<nums.size(); i++){
+                    nums[i] = 0;
                 }
             }
         };
+        
+        //O(N) + O(X) + O(N-X) ~ O(2*N), where N = total no. of elements,
+        // X = no. of non-zero elements, and N-X = total no. of zeros.
+
+                
+        //Solution 3 : Time complexity: O(n).
+        // Space complexity: O(1).
+            class Solution {
+                public:
+                    void moveZeroes(vector<int>& nums) {
+                        int j = 0;
+                        for (int i = 0; i < nums.size(); i++) {
+                            if (nums[i] != 0) {
+                                swap(nums[i], nums[j]);
+                                j++;
+                            }
+                        }
+                    }
+                };

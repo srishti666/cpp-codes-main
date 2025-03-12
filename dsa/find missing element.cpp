8210306ecaa3a8b9a1840a63a2019ebc658dd99f
srishti <srishtimanna6@gmail.com>
2025-03-12 22:38:07 +0530

    O(2N)

--- dsa/find missing element.cpp
+++ dsa/find missing element.cpp
@@ -19,5 +19,27 @@ class Solution {
             return -1;
         }
     };
+
+    // solution 2: TC: O(N) + O(N) ~ O(2*N)
+
+    #include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n + 1, 0);
+
+        for(int i = 0; i < n; i++) {
+            hash[nums[i]]++;
+        }
+
+        for(int i = 0; i <= n; i++) {
+            if(hash[i] == 0) return i;
+        }
+        return -1;
+    }
+};
     
     

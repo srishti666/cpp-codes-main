eacfee69a9e605d7605bed022ff8b567ec82412d
srishti <srishtimanna6@gmail.com>
2025-03-14 21:59:18 +0530

    .

--- dsa/find missing element.cpp
+++ dsa/find missing element.cpp
@@ -46,17 +46,21 @@ public:
 // XOR of two same numbers is always 0 i.e. a ^ a = 0. ←Property 1.
 // XOR of a number with 0 will result in the number itself i.e. 0 ^ a = a.  ←Property 2
 
-int missingNumber(vector<int>&a, int N) {
-
-    int xor1 = 0, xor2 = 0;
-
-    for (int i = 0; i < N - 1; i++) {
-        xor2 = xor2 ^ a[i]; // XOR of array elements
-        xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
-    }
-    xor1 = xor1 ^ N; //XOR up to [1...N]
-
-    return (xor1 ^ xor2); // the missing number
-}
+class Solution {
+    public:
+        int missingNumber(vector<int>& nums) {
+            int N = nums.size();
+            int xor1 = 0, xor2 = 0;
+    
+            for (int i = 0; i < N; i++) {
+                xor2 = xor2 ^ nums[i]; // XOR of array elements
+                xor1 = xor1 ^ i; // XOR up to [0...N-1]
+            }
+    
+            xor1 = xor1 ^ N; // XOR up to [0...N]
+    
+            return (xor1 ^ xor2); // missing number
+        }
+    };
     
     

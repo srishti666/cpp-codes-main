d4ba3c897ad8b08efd40d2e34deb2234bdaf4f29
srishti <srishtimanna6@gmail.com>
2025-03-24 10:09:10 +0530

    .

--- dsa/majority element.cpp
+++ dsa/majority element.cpp
@@ -20,3 +20,24 @@ class Solution {
             return 0;
         }
     };
+
+
+    //Moore's voting algorithm 
+    //TC: O(n)
+
+class Solution {
+    public:
+        int majorityElement(vector<int>& nums) {
+            int freq = 0, ans = 0;
+            for(int i=0; i<nums.size(); i++){
+                if(freq == 0){
+                    ans = nums[i];
+                }
+                if(ans == nums[i]) {
+                    freq++;
+                }
+                else freq--;
+            }
+        return ans;
+        }}
+

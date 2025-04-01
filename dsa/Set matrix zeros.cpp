3d6cf30abea8299c2404357a3feceed4f3a9308d
srishti <srishtimanna6@gmail.com>
2025-04-01 12:08:26 +0530

    better solution

--- dsa/Set matrix zeros.cpp
+++ dsa/Set matrix zeros.cpp
@@ -45,3 +45,34 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
 
     return matrix;
 }
+
+//better solution
+//TC: O(n*m) + O(n*m) = O(2*(n*m))
+
+class Solution {
+    public:
+        void setZeroes(vector<vector<int>>& matrix) {
+    
+            //size of the matrix rows and cols
+            int n = matrix.size();
+            int m = matrix[0].size();
+    
+            int row[n] = {0};
+            int col[m] = {0};
+    
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(matrix[i][j] == 0) row[i] = 1;col[j] = 1;
+                }
+            }
+    
+            for(int i=0; i<n; i++){
+                for(int j=0; j<m; j++){
+                    if(row[i] || col[j]){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+            return matrix;
+        }
+    };

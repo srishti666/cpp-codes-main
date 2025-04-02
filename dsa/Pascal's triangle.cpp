92a07f5473871efb61d0457887a3255214b83f65
Srishti Manna <85846340+srishti666@users.noreply.github.com>
2025-04-02 22:46:17 +0530

    Add files via upload

---
+++ dsa/Pascal's triangle.cpp
@@ -0,0 +1,18 @@
+//question: https://leetcode.com/problems/pascals-triangle/
+
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+int pascalTriangle(int r, int c) {
+    int element = nCr(r - 1, c - 1);
+    return element;
+}

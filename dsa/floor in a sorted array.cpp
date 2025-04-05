96481ff29ca46be74f8ee47fb90c18424611a19d
srishti <srishtimanna6@gmail.com>
2025-04-06 00:30:06 +0530

    brute force

--- dsa/floor in a sorted array.cpp
+++ dsa/floor in a sorted array.cpp
@@ -1,4 +1,4 @@
-//question: 
+//question: https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array
 
 //TC: O(n)
 class Solution {

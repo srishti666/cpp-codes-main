73b4889563f0f6ae00aa0def24937a5b7a1d1427
srishti <srishtimanna6@gmail.com>
2025-04-05 10:50:55 +0530

    recursive

--- dsa/binary search.cpp
+++ dsa/binary search.cpp
@@ -17,3 +17,23 @@ class Solution {
             return -1;
         }
     };
+
+
+    //recursive approach: TC: O(logn)
+
+class Solution {
+    public:
+    
+        int bs(vector<int> &nums, int low, int high, int target){
+            if(low > high) return -1;
+            int mid = (low + high)/2;
+            if(nums[mid] == target) return mid;
+            else if(target > nums[mid]) 
+            return bs(nums, mid+1, high, target);
+            else return bs(nums, low , mid-1, target);
+        }
+    
+        int search(vector<int>& nums, int target) {
+            return bs(nums,0, nums.size() - 1,  target);
+        }
+    };

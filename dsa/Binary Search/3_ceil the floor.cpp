9f130570ed467f0fb6702c909832c00246bb873d
srishti <srishtimanna6@gmail.com>
2025-04-08 07:36:51 +0530

    binary search

--- dsa/Binary Search/3_ceil the floor.cpp
+++ dsa/Binary Search/3_ceil the floor.cpp
@@ -28,3 +28,35 @@ class Solution {
           
       }
   };
+
+  //using binary search
+
+  class Solution {
+    public:
+      vector<int> getFloorAndCeil(int x, vector<int> &arr) {
+          sort(arr.begin(), arr.end());
+          int n = arr.size();
+          int low = 0; 
+          int high = n -1;
+          int floor = -1;
+          int ceil = -1;
+          
+          while(low <= high){
+              int mid = (low+high)/2;
+              if(arr[mid] == x) {
+                  return {x, x};
+              }
+              else if(arr[mid] <= x)  {
+                  floor = arr[mid];
+                  low = mid + 1;
+              }
+              else if(arr[mid] >= x){
+                  ceil = arr[mid];
+                  high = mid - 1;
+              }
+                  
+          }
+          return {floor, ceil};
+      }
+      
+  };

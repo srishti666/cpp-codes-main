044de4c3cfc0cb5ae6c95ba9f331bb2b3d322c6c
srishti <srishtimanna6@gmail.com>
2025-04-09 11:23:59 +0530

    optimised

--- dsa/Binary Search/5_first and last occurences in an aray.cpp
+++ dsa/Binary Search/5_first and last occurences in an aray.cpp
@@ -15,3 +15,52 @@ class Solution {
             return {first, last};
         }
     };
+
+
+    //Optimised approach
+    //TC: 2*O(logn)
+
+    class Solution {
+        public:
+            int firstOccurence(vector<int>& nums, int target){
+                int n = nums.size();
+                int low = 0;
+                int high = n-1;
+                int first = -1;
+        
+                while(low <= high){
+                    int mid = (low + high)/2;
+                    if(nums[mid] == target){
+                        first = mid;
+                        high =  mid - 1;
+                    }
+                    else if(nums[mid] < target)low = mid + 1;
+                    else high = mid - 1;
+                }
+                return first;
+            }
+        //last occurence
+            int lastOccurence(vector<int>& nums, int target){
+                int n = nums.size();
+                int low = 0;
+                int high = n-1;
+                int last = -1;
+        
+                while(low <= high){
+                    int mid = (low + high)/2;
+                    if(nums[mid] == target){
+                        last = mid;
+                        low =  mid + 1;
+                    }
+                    else if(nums[mid] < target)low = mid + 1;
+                    else high = mid - 1;
+                }
+                return last;
+            }
+            vector<int> searchRange(vector<int>& nums, int target) {
+                int first = firstOccurence(nums, target);
+                if(first == -1) return { -1, -1};
+                int last = lastOccurence(nums, target);
+                return {first, last};
+            }
+        };

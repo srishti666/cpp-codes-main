8372430e5c6fd746422306255315c548fd2bc4f5
srishti <srishtimanna6@gmail.com>
2025-04-09 08:07:27 +0530

    brute force

---
+++ dsa/Binary Search/5_first and last occurences in an aray.cpp
@@ -0,0 +1,17 @@
+//question: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+//TC: O(n) using linear search
+class Solution {
+    public:
+        vector<int> searchRange(vector<int>& nums, int target) {
+            int first = -1;
+            int last = -1;
+            for(int i=0; i<nums.size(); i++){
+                if(nums[i] == target){
+                    if(first == -1){first = i;} 
+                    last = i;
+                }
+            }
+            return {first, last};
+        }
+    };

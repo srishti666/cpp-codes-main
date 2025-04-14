1f0fb805f93e36ac976d628755825018db86dbe9
srishti <srishtimanna6@gmail.com>
2025-04-14 16:01:01 +0530

    brute force

--- dsa/Binary Search/12_find peak element.cpp
+++ dsa/Binary Search/12_find peak element.cpp
@@ -1,5 +1,20 @@
 //question: https://leetcode.com/problems/find-peak-element/description/
 
+//TC: O(n)
+class Solution {
+    public:
+        int findPeakElement(vector<int>& nums) {
+            int n = nums.size();
+            for(int i=0; i<n-1; i++){
+                if(nums[i] > nums[i+1]){
+                    return i;
+                }
+            }
+            return n-1;
+        }
+    };
+
+
 //TC: O(logn)
 class Solution {
     public:

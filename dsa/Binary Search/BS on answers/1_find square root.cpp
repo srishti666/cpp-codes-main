185fa50e87ef70debe9a8988884a98b29555d882
srishti <srishtimanna6@gmail.com>
2025-04-15 10:43:48 +0530

    optimized-binary

--- dsa/Binary Search/BS on answers/1_find square root.cpp
+++ dsa/Binary Search/BS on answers/1_find square root.cpp
@@ -32,3 +32,29 @@ class Solution {
       }
     }
 
+    //binary search  TC: O(logn)
+
+    class Solution {
+        public:
+          int floorSqrt(int n) {
+             int low = 0;
+             int high = n;
+             int ans = 1;
+             
+             while(low <= high){
+                 int mid = (low + high)/2;
+                 if(mid * mid <= n){
+                     ans = mid;
+                     low = mid + 1;
+                 }
+                 else{
+                     high = mid - 1;
+                 }
+                 
+             }
+             return ans;
+             
+          }
+      };
+      
+
